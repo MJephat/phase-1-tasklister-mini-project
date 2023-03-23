3bed50363a97804e93534938066f38359d14b3f5
MJephat <mukundijephat22@gmail.com>
2023-03-23 19:16:26 +0300

    will be revisited

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.querySelector('form').addEventListener('SUBMIT', (e) =>{
+//     e.preventDefault();
+//     console.log(e);
+//   })
+// });
+
+//adding an event listener
+ document.querySelector('form').addEventListener('mouseover', function (){
+  console.log('Mambo yamechemka')
+})
+
+document.querySelector('form').addEventListener('mouseover', e => console.log(e))
+
+//creating an alert.
+document.querySelector('.btn').addEventListener
+('click', () => alert('i am alert'));
+
+
+let addbutton = document.querySelector('addbutton');
+
+addbutton.onclick = function () {
+  let input = document.querySelector('input').value;
+  console.log(input);
+}
